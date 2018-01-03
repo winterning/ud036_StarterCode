cae9e31ce020418482ff238afee655940a2b24b6
xiaoning.feng <xiaoning.feng@yingzt.com>
2018-01-03 23:07:29 +0800

    python小程序

---
+++ media.py
@@ -0,0 +1,7 @@
+import webbrowser
+class Movie:
+    def __init__(self,movie_title,poster_url,youtube_url):
+        self.title=movie_title
+        self.poster_image_url=poster_url
+        self.trailer_youtube_url=youtube_url
+
---
+++ movieList.py
@@ -0,0 +1,10 @@
+import media
+import fresh_tomatoes
+my_father_and_mother = media.Movie("我的父亲母亲","http://t3.baidu.com/it/u=1468520311,48859012&fm=20","http://baishi.baidu.com/watch/9020637406734644711.html")
+
+my_dear = media.Movie("亲爱的","http://t1.baidu.com/it/u=209378971,2921209751&fm=20","http://baishi.baidu.com/watch/5940905633953268435.html")
+
+
+movieList = [my_father_and_mother,my_dear]
+
+fresh_tomatoes.open_movies_page(movieList)
