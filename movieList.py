c0ff45aeab098cc8fa09e4e1dd8d3a8e56331a9e
xiaoning.feng <xiaoning.feng@yingzt.com>
2018-01-04 00:11:44 +0800

    youtube资源

--- movieList.py
+++ movieList.py
@@ -1,9 +1,29 @@
 import media
 import fresh_tomatoes
-my_father_and_mother = media.Movie("my_father_and_mother","http://t3.baidu.com/it/u=1468520311,48859012&fm=20","http://baishi.baidu.com/watch/9020637406734644711.html")
+movieOne = media.Movie("All NEW Ranked Changes, Skins & Events EXPLAINED",
+                                   "https://i.ytimg.com/vi/Bkd0jWUHIpI/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFDyq4qpAwsIARUAAIZCGAFwAQ==&rs=AOn4CLB0IbW3eMEVbflb4pBNvSuJWlf0dw",
+                                   "https://www.youtube.com/watch?v=Bkd0jWUHIpI")
 
-my_dear = media.Movie("my_dear","http://t1.baidu.com/it/u=209378971,2921209751&fm=20","http://baishi.baidu.com/watch/5940905633953268435.html")
+movieTwo = media.Movie("How PROS Counter Bastion Cheese Comp!",
+                       "https://i.ytimg.com/vi/-i14IPf67UY/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFDyq4qpAwsIARUAAIZCGAFwAQ==&rs=AOn4CLCH9mJJ9HJwXh7kgAb3pyqsNVRwBg",
+                       "https://www.youtube.com/watch?v=-i14IPf67UY&t=3s")
 
-movieList = [my_father_and_mother,my_dear]
+movieThree = media.Movie("Dunkirk - Trailer 1 [HD]",
+                       "https://i.ytimg.com/vi/F-eMt3SrfFU/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFDyq4qpAwsIARUAAIZCGAFwAQ==&rs=AOn4CLCSB1BwLMAt6XmH2oQjWN8IYOXfZw",
+                       "https://www.youtube.com/watch?v=F-eMt3SrfFU&t=37s")
+
+movieFour = media.Movie("Jeff Kaplan trolls entire overwatch stream",
+                       "https://i.ytimg.com/vi/SjDtOMAULLg/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFDyq4qpAwsIARUAAIZCGAFwAQ==&rs=AOn4CLCFD8WZYjvcrQkR-Sxt88pxrJAPJA",
+                       "https://www.youtube.com/watch?v=SjDtOMAULLg")
+
+movieFive = media.Movie("Streamers vs Pros",
+                       "https://i.ytimg.com/vi/Kk54eP4OTNw/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFDyq4qpAwsIARUAAIZCGAFwAQ==&rs=AOn4CLBOWbcBKMzRNM59GJgDpyTEVYXCOw",
+                       "https://www.youtube.com/watch?v=Kk54eP4OTNw")
+
+movieSix = media.Movie("Greatest Team Kills",
+                       "https://i.ytimg.com/vi/NFKZGWUOreY/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFDyq4qpAwsIARUAAIZCGAFwAQ==&rs=AOn4CLBxhG0OppAD9_GTHSeitpZwERyV2Q",
+                       "https://www.youtube.com/watch?v=NFKZGWUOreY")
+
+movieList = [movieOne,movieTwo,movieThree,movieFour,movieFive,movieSix]
 
 fresh_tomatoes.open_movies_page(movieList)
