ba00149215b52313fcdb64eee93fb977ec1f1966
xiaoning.feng <xiaoning.feng@yingzt.com>
2018-01-04 22:51:15 +0800

    修改图片地址

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,27 +2,27 @@
 import media
 import fresh_tomatoes
 movieOne = media.Movie("WILD CHILD",
-                       "https://i.ytimg.com/vi/v2oC2vJailQ/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFDyq4qpAwsIARUAAIZCGAFwAQ==&rs=AOn4CLBRKvnEOZVhlG9lF9-wYm6t0HEtDg",
+                       "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2506466229.jpg",
                        "https://www.youtube.com/watch?v=v2oC2vJailQ")
 
 movieTwo = media.Movie("Dunkirk",
-                       "https://i.ytimg.com/vi/F-eMt3SrfFU/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFDyq4qpAwsIARUAAIZCGAFwAQ==&rs=AOn4CLCSB1BwLMAt6XmH2oQjWN8IYOXfZw",
+                       "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2506139427.jpg",
                        "https://www.youtube.com/watch?v=F-eMt3SrfFU&t=46s")
 
 movieThree = media.Movie("ALGORITHM: The Hacker",
-                       "https://i.ytimg.com/vi/6qpudAhYhpc/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFDyq4qpAwsIARUAAIZCGAFwAQ==&rs=AOn4CLCnDfwpDHVk9JWmB6vNxGXoEjI4yQ",
+                       "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2507227732.jpg",
                        "https://www.youtube.com/watch?v=6qpudAhYhpc")
 
 movieFour = media.Movie("THE MARS UNDERGROUND ",
-                       "https://i.ytimg.com/vi/tcTZvNLL0-w/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFDyq4qpAwsIARUAAIZCGAFwAQ==&rs=AOn4CLBJVC2m8lPFJO0XwYHrfbW1a3UFDw",
+                       "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2507022339.jpg",
                        "https://www.youtube.com/watch?v=tcTZvNLL0-w")
 
 movieFive = media.Movie("Last Passenger",
-                       "https://i.ytimg.com/vi/QRriteWU9UI/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFDyq4qpAwsIARUAAIZCGAFwAQ==&rs=AOn4CLCtntMPABvSO4lk3HsbjzpAGRuPYQ",
+                       "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2503997609.jpg",
                        "https://www.youtube.com/watch?v=QRriteWU9UI")
 
 movieSix = media.Movie("英伦对决",
-                       "https://i.ytimg.com/vi/r_rSAbYyIq0/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFDyq4qpAwsIARUAAIZCGAFwAQ==&rs=AOn4CLC3m3cfQoxUpM82Ap4qt8ODVxJ2Pg",
+                       "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2499135561.jpg",
                        "https://www.youtube.com/watch?v=r_rSAbYyIq0")
 
 movieList = [movieOne,movieTwo,movieThree,movieFour,movieFive,movieSix]
