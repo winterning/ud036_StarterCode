fc6a383fe26fdf6337a649b1a27d4a2bf43a49b1
xiaoning.feng <xiaoning.feng@yingzt.com>
2018-01-04 23:33:07 +0800

    修改文件风格

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ud036_StarterCode
 Source code for a Movie Trailer website.
+
+python entertainment_center.py
--- media.py
+++ media.py
@@ -1,7 +1,5 @@
-import webbrowser
 class Movie:
-    def __init__(self,movie_title,poster_url,youtube_url):
-        self.title=movie_title
-        self.poster_image_url=poster_url
-        self.trailer_youtube_url=youtube_url
-
+    def __init__(self, movie_title, poster_url, youtube_url):
+        self.title = movie_title
+        self.poster_image_url = poster_url
+        self.trailer_youtube_url = youtube_url
