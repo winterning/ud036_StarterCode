11cee2ab301e1b20492bd49b465663b66739ad9f
xiaoning.feng <xiaoning.feng@yingzt.com>
2018-01-09 13:45:50 +0800

    添加注释

--- media.py
+++ media.py
@@ -1,5 +1,7 @@
 class Movie:
+    """ create the class of Movie """
     def __init__(self, movie_title, poster_url, youtube_url):
+        """ Initialize the attributes of the class Movie"""
         self.title = movie_title
         self.poster_image_url = poster_url
         self.trailer_youtube_url = youtube_url
